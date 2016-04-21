651c465c5f14627f23c0624448ff811bee8633e1
Dev-Liang <wallelj0909@gmail.com>
2016-04-21 13:47:45 +0800

    增加了Push

--- LJAppStandard/LJAppStandard/Common/AppDelegate.m
+++ LJAppStandard/LJAppStandard/Common/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "LJTabBarController.h"
 #import "LJNetWorkingTools.h"
+#import "JPUSHService.h"
 @interface AppDelegate ()
 
 @end
@@ -22,6 +23,28 @@
     [self.window makeKeyAndVisible];
     
     [self checkNetWork];
+    
+    
+    // 注册
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+        //可以添加自定义categories iOS8 新特性，例如快速回复
+        [JPUSHService registerForRemoteNotificationTypes:(UIUserNotificationTypeBadge |
+                                                          UIUserNotificationTypeSound |
+                                                          UIUserNotificationTypeAlert)
+                                              categories:nil];
+    } else {
+        //categories 必须为nil
+        [JPUSHService registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+                                                          UIRemoteNotificationTypeSound |
+                                                          UIRemoteNotificationTypeAlert)
+                                              categories:nil];
+    }
+    
+    [JPUSHService setupWithOption:launchOptions appKey:@"3d02c654219bbf8de2849a22"
+                          channel:@"appstore"
+                 apsForProduction:NO // 如果为开发状态,设置为 NO; 如果为生产状态,应改为 YES.
+            advertisingIdentifier:nil];// 广告 不用设置为nil
+    
     return YES;
 }
 
@@ -29,6 +52,23 @@
 - (void)checkNetWork{
     [LJNetWorkingTools checkNetWorkStatus];
     [LJNetWorkingTools addNetWorkChangeEveryTime];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    /// Required - 注册 DeviceToken
+    [JPUSHService registerDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    // 收到通知就触发
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+    [JPUSHService resetBadge]; // 重置 脚标
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    // error 处理
+    NSLog(@"eorror");
 }
 
 @end
